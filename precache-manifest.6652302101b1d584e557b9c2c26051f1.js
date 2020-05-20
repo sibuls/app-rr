fb351c0d9c57abaa04354af66690e96ab6edab91
Sibuls <sebo.orlowski@gmail.com>
2020-05-20 18:24:50 +0200

    Updates

---
+++ precache-manifest.6652302101b1d584e557b9c2c26051f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66dac5597fa4f847735d8964188f3df1",
+    "url": "/app-rr/index.html"
+  },
+  {
+    "revision": "0681d5298239d07477f3",
+    "url": "/app-rr/static/css/main.fd9dce09.chunk.css"
+  },
+  {
+    "revision": "647df8617dedccedb855",
+    "url": "/app-rr/static/js/2.cc2acc9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-rr/static/js/2.cc2acc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0681d5298239d07477f3",
+    "url": "/app-rr/static/js/main.132d0fdf.chunk.js"
+  },
+  {
+    "revision": "496b78500face617a82e",
+    "url": "/app-rr/static/js/runtime-main.46f84603.js"
+  },
+  {
+    "revision": "3fc56d027cdc7e7a28e5c97837d17fae",
+    "url": "/app-rr/static/media/header1.3fc56d02.jpg"
+  },
+  {
+    "revision": "c581f3853032afd64f9576edb6c98df4",
+    "url": "/app-rr/static/media/header2.c581f385.jpg"
+  },
+  {
+    "revision": "3cf154d415924bc3ed6dec64b9e819c0",
+    "url": "/app-rr/static/media/header3.3cf154d4.jpg"
+  },
+  {
+    "revision": "29ae1727a3de6ddc2157bbc6933de404",
+    "url": "/app-rr/static/media/header4.29ae1727.jpg"
+  }
+]);
